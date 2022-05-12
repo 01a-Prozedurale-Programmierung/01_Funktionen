df14ba8a081e803be9bcd1fdf292799bd46243bd
Peter <schmidt.multimedia@gmail.com>
2022-05-12 15:11:08 +0200

    Funktionen I

--- script_01.js
+++ script_01.js
@@ -4,3 +4,35 @@
 
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log("Grizabella");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+
+function ausgabeNamen() {
+    let firstName = "David"; 
+    console.log("Hallo " + firstName + "!"); 
+}
+
+// console.log(firstName); // .. stays in Vegas!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Bernhard");  // Args ..
+ausgabeNamen2("Mohanned");
+ausgabeNamen2("Bianca");
+
+function ausgabeNamen2(firstName) { // Parameter
+    console.log("Hallo " + firstName + "!"); 
+}
+
